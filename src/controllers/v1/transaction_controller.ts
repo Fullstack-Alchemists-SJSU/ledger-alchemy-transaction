4deb6114ffc32ca7d1cde9a21f530a0f0187f8a8
ShivamHasurkarSJSU <shivamravindra.hasurkar@sjsu.edu>
2023-11-28 17:18:26 -0800

    sync transactions implemented

--- src/controllers/v1/transaction_controller.ts
+++ src/controllers/v1/transaction_controller.ts
@@ -9,7 +9,7 @@ sequelize.sync({ alter: true }).then(() => {
 });
 
 // To fetch and store transactions
-export const fetchUserTransaction = async (req: any, res: any) => {
+export const getUserTransaction = async (req: any, res: any) => {
     const { user_id, access_token } = req.body;
 
     try {
@@ -18,11 +18,11 @@ export const fetchUserTransaction = async (req: any, res: any) => {
         });
 
         for (const account of bankAccounts) {
-            // Fetch transactions from Plaid
+            // Get transactions from Plaid
             const response = await plaidClient.transactionsGet({
                 access_token: access_token,
                 start_date: '2021-01-01', // Dates to be fixed for a month period or to be taken as input from user.
-                end_date: '2023-11-15',
+                end_date: new Date().toISOString().split('T')[0], // Today's date in YYYY-MM-DD format
                 options: {
                     account_ids: ['3evnlyMGeWCNEWkv7pNqIW63MKeryBuZvm4KZ', '3evnlyMGeWCNEWkv7pNqIW63MKeryBuZvm4KZ'] //[account.account_id]
                 }
@@ -32,7 +32,7 @@ export const fetchUserTransaction = async (req: any, res: any) => {
             console.log(transactions);
 
             // Store each transaction in the database
-            await storeTransactions(transactions);
+            await storeGetTransactions(transactions);
         }
 
         res.send('Transactions fetched and stored successfully.');
@@ -42,36 +42,99 @@ export const fetchUserTransaction = async (req: any, res: any) => {
     }
 };
 
-async function storeTransactions(transactions: any[]): Promise<void> {
+export const syncUserTransaction = async (req: any, res: any) => {
+    const { user_id, access_token } = req.body;
+
+    try {
+        const bankAccounts = await BankAccount.findAll({
+            where: { user_id: user_id }
+        });
+
+        for (const account of bankAccounts) {
+            let cursor = ''; // Start with null for the first sync
+            let hasMore = true;
+
+            while (hasMore) {
+                const response = await plaidClient.transactionsSync({
+                    access_token: access_token,
+                    cursor: cursor,
+                    count: 100 // optional, adjust as needed
+                });
+
+                const { added, modified, removed, has_more, next_cursor } = response.data;
+
+                // Process and store the transactions
+                // Example function to handle storage
+                await storeSyncTransactions(added, modified, removed);
+
+                hasMore = has_more;
+                cursor = next_cursor;
+            }
+        }
+
+        res.send('Transactions synced and stored successfully.');
+
+    } catch (error) {
+        console.error('Error syncing transactions:', error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+async function storeGetTransactions(transactions: any[]): Promise<void> {
     for (const trans of transactions) {
-        await Transaction.create({
-            transaction_id: trans.transaction_id,
-            account_id: trans.account_id,
-            amount: trans.amount,
-            date: new Date(trans.date),
-            name: trans.name,
-            payment_channel: trans.payment_channel,
-            transaction_type: trans.transaction_type,
-            category: trans.category,
-            category_id: trans.category_id,
-            pending: trans.pending,
-            transaction_code: trans.transaction_code,
-            iso_currency_code: trans.iso_currency_code,
-            unofficial_currency_code: trans.unofficial_currency_code,
-            merchant_name: trans.merchant_name,
-            authorized_date: trans.authorized_date ? new Date(trans.authorized_date) : null,
-            authorized_datetime: trans.authorized_datetime ? new Date(trans.authorized_datetime) : null,
-            location: JSON.stringify(trans.location),
-            payment_meta: JSON.stringify(trans.payment_meta),
-            personal_finance_category_primary: trans.personal_finance_category?.primary,
-            personal_finance_category_detailed: trans.personal_finance_category?.detailed,
-            personal_finance_category_confidence_level: trans.personal_finance_category?.confidence_level,
-            personal_finance_category_icon_url: trans.personal_finance_category?.icon_url,
-            logo_url: trans.logo_url,
-            merchant_entity_id: trans.merchant_entity_id,
-            check_number: trans.check_number,
-            website: trans.website,
-            counterparties: JSON.stringify(trans.counterparties)
+        await Transaction.create(mapTransactionFields(trans));
+    }
+}
+
+async function storeSyncTransactions(added: any[], modified: any[], removed: any[]): Promise<void> {
+    // Handle added transactions
+    for (const trans of added) {
+        await Transaction.create(mapTransactionFields(trans));
+    }
+
+    // Handle removed transactions
+    for (const trans of removed) {
+        await Transaction.destroy({
+            where: { transaction_id: trans.transaction_id }
         });
     }
+
+    // Handle modified transactions
+    for (const trans of modified) {
+        await Transaction.update(mapTransactionFields(trans), {
+            where: { transaction_id: trans.transaction_id }
+        });
+    }
+}
+
+function mapTransactionFields(trans: any) {
+    return {
+        transaction_id: trans.transaction_id,
+        account_id: trans.account_id,
+        amount: trans.amount,
+        date: new Date(trans.date),
+        name: trans.name,
+        payment_channel: trans.payment_channel,
+        transaction_type: trans.transaction_type,
+        category: trans.category,
+        category_id: trans.category_id,
+        pending: trans.pending,
+        transaction_code: trans.transaction_code,
+        iso_currency_code: trans.iso_currency_code,
+        unofficial_currency_code: trans.unofficial_currency_code,
+        merchant_name: trans.merchant_name,
+        authorized_date: trans.authorized_date ? new Date(trans.authorized_date) : null,
+        authorized_datetime: trans.authorized_datetime ? new Date(trans.authorized_datetime) : null,
+        location: JSON.stringify(trans.location),
+        payment_meta: JSON.stringify(trans.payment_meta),
+        personal_finance_category_primary: trans.personal_finance_category?.primary,
+        personal_finance_category_detailed: trans.personal_finance_category?.detailed,
+        personal_finance_category_confidence_level: trans.personal_finance_category?.confidence_level,
+        personal_finance_category_icon_url: trans.personal_finance_category?.icon_url,
+        logo_url: trans.logo_url,
+        merchant_entity_id: trans.merchant_entity_id,
+        check_number: trans.check_number,
+        website: trans.website,
+        counterparties: JSON.stringify(trans.counterparties)
+    };
 }
